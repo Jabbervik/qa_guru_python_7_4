fed1967797993e28a64aabae92fe6bf5c242b3f8
Viktor Sorokin <jabervik@gmail.com>
2025-02-13 16:04:38 +0300

    Add tasks 1-5

--- test_homework.py
+++ test_homework.py
@@ -1,4 +1,5 @@
-
+import math
+import random
 
 def test_greeting():
     """
@@ -7,7 +8,7 @@ def test_greeting():
     name = "Анна"
     age = 25
     # TODO Сформируйте нужную строку
-    output = ""
+    output = f"{'Привет, ' + name + '! '}{'Тебе ' + str(age) + ' лет.'}"
 
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
@@ -23,11 +24,12 @@ def test_rectangle():
     # TODO сосчитайте периметр
     perimeter = 0
 
+    perimeter = 2 * (a + b)
     assert perimeter == 60
 
     # TODO сосчитайте площадь
     area = 0
-
+    area = a * b
     assert area == 200
 
 
@@ -39,12 +41,12 @@ def test_circle():
     r = 23
     # TODO сосчитайте площадь
     area = 0
-
+    area = math.pi * r ** 2
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
     length = 0
-
+    length = 2 * math.pi * r
     assert length == 144.51326206513048
 
 
@@ -53,7 +55,8 @@ def test_random_list():
     Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
     """
     # TODO создайте список
-    l = []
+    l = [random.randint(1, 100) for _ in range(10)]
+    l.sort()
 
     assert len(l) == 10
     assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
@@ -65,7 +68,7 @@ def test_unique_elements():
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
     # TODO удалите повторяющиеся элементы
-
+    l = list(set(l))
     assert isinstance(l, list)
     assert len(l) == 10
     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -79,8 +82,11 @@ def test_dicts():
     """
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
+    zipped_values = zip(first, second)
     # TODO создайте словарь
-    d = {}
+    d = {
+        list(zipped_values)[i][0]: list(zipped_values)[i][1] for i in range(len(list(zipped_values)))
+    }
 
     assert isinstance(d, dict)
     assert len(d) == 5
